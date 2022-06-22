b8b8735e0b30a79ee92d5d45760f5de44fee1662
I-Fan Chen <ifanchen@amazon.com>
2022-06-22 12:07:21 -0700

    Add LISCENCE file and headers with some content clean up

--- data/espnet/simpleoier_librispeech_asr_train_asr_conformer7_hubert_ll60k_large_raw_en_bpe5000_sp/eval.sh
+++ data/espnet/simpleoier_librispeech_asr_train_asr_conformer7_hubert_ll60k_large_raw_en_bpe5000_sp/eval.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+# Copyright 2022 Amazon.com, Inc. and its affiliates. All Rights Reserved.
+#  
+# SPDX-License-Identifier: LicenseRef-.amazon.com.-AmznSL-1.0
+# 
+# Licensed under the Amazon Software License (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+# 
+#   http://aws.amazon.com/asl/
+# 
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
 
 # Use sclite to compute WER
 # Make sure you've install SCTK https://github.com/usnistgov/SCTK
